df7e57501c88c3a74c879e05b671eeb3972bb63e
Umut <umutekerdiker@gmail.com>
2022-10-30 20:42:04 +0000

    Add heroku port

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    sameSite: "none"
+    sameSite: "none",
+    secure: true
   }
 }));
 
@@ -232,6 +233,11 @@ app.post("/login", passport.authenticate("local"), function(req, res) {
   })
 });
 
-app.listen(3000, function(req, res) {
-  console.log("Server started on port 3000.");
-})
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+};
+
+app.listen(port, function() {
+  console.log("Server has started successfully.");
+});
