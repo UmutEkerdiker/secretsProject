c9624134352ee82a57039a159a7fb19fce926c44
Umut <umutekerdiker@gmail.com>
2022-10-30 22:34:46 +0000

    Fix atlas key

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(passport.session());
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/userDB');
+  await mongoose.connect(process.env.ATLAS_KEY);
 };
 
 //create user schema and include Google and Facebook ID's.
