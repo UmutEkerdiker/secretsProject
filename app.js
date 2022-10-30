ff4446044d2acd975636fe95e1cbec7b46b3faa0
Umut <umutekerdiker@gmail.com>
2022-10-30 19:50:46 +0000

    Fix google auth

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
